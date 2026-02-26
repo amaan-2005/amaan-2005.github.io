f51583ec38dc59e07a6d5b9e94f6c4d42adf2f96
MURSITH AMAAN <amaanmus123@gmail.com>
2026-02-26 18:01:43 +0530

    Add popup and auto grid logic

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// PROFILE POPUP TOGGLE
+const profilePic = document.getElementById("profilePic");
+const profilePopup = document.getElementById("profilePopup");
 
+profilePic.addEventListener("click", () => {
+  profilePopup.style.display =
+    profilePopup.style.display === "block" ? "none" : "block";
+});
+
+window.addEventListener("click", function(e) {
+  if (!profilePic.contains(e.target) && !profilePopup.contains(e.target)) {
+    profilePopup.style.display = "none";
+  }
+});
+
+// AUTO GRID GENERATOR
+const grid = document.getElementById("postGrid");
+
+if (typeof posts !== "undefined") {
+  posts
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .forEach(post => {
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <h2 onclick="location.href='${post.file}'">${post.title}</h2>
+        <div class="date">${post.date}</div>
+      `;
+
+      grid.appendChild(card);
+    });
+}
